5f2e56bd4462e5187050e2c2e83ab05fb7dc1a8c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomLabelsText/Form1.vb
@@ -0,0 +1,20 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+
+Namespace CustomLabelsText
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			Dim coll As CustomAxisLabelCollection = (CType(Me.chartControl1.Diagram, XYDiagram)).AxisX.CustomLabels
+			coll(0).Name = DateTime.Today.DayOfWeek.ToString()
+			coll(1).Name = DateTime.Today.AddDays(4).DayOfWeek.ToString()
+		End Sub
+
+	End Class
+End Namespace
