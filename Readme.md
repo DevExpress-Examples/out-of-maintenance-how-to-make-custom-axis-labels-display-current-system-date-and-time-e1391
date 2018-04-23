656724904fd6053739638da61bf0ac84fa08eb79
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to make custom axis labels display current system date and time
+
+
+<p>This example demonstrates how to change a text of custom <a href="http://devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument5804.htm">axis labels</a>  at runtime. In particular, it demonstrates how to make them display the current system date and time.</p><p>Note that to access the custom labels collection, you should first access its parent <a href="http://devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument6016.htm">axis</a>. That can be done only if you cast your chart's <a href="http://devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument6017.htm">diagram</a> object to the appropriate diagram type.</p>
+
+<br/>
+
+
---
+++ VB/CustomLabelsText/Form1.Designer.vb
@@ -0,0 +1,106 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace CustomLabelsText
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
+			Dim customAxisLabel1 As New DevExpress.XtraCharts.CustomAxisLabel()
+			Dim customAxisLabel2 As New DevExpress.XtraCharts.CustomAxisLabel()
+			Dim series1 As New DevExpress.XtraCharts.Series()
+			Dim pointSeriesLabel1 As New DevExpress.XtraCharts.PointSeriesLabel()
+			Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("A", New Object() { (CObj(1))})
+			Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("B", New Object() { (CObj(3))})
+			Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("C", New Object() { (CObj(2))})
+			Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("D", New Object() { (CObj(4))})
+			Dim seriesPoint5 As New DevExpress.XtraCharts.SeriesPoint("E", New Object() { (CObj(1))})
+			Dim splineAreaSeriesView1 As New DevExpress.XtraCharts.SplineAreaSeriesView()
+			Dim sideBySideBarSeriesLabel1 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(series1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(splineAreaSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' chartControl1
+			' 
+			customAxisLabel1.AxisValueSerializable = "A"
+			customAxisLabel1.Name = "Start"
+			customAxisLabel2.AxisValueSerializable = "E"
+			customAxisLabel2.Name = "End"
+			xyDiagram1.AxisX.CustomLabels.AddRange(New DevExpress.XtraCharts.CustomAxisLabel() { customAxisLabel1, customAxisLabel2})
+			xyDiagram1.AxisX.GridLines.MinorVisible = True
+			xyDiagram1.AxisX.GridLines.Visible = True
+			xyDiagram1.AxisX.Range.ScrollingRange.SideMarginsEnabled = False
+			xyDiagram1.AxisX.Range.SideMarginsEnabled = False
+			xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+			xyDiagram1.AxisY.GridLines.Visible = False
+			xyDiagram1.AxisY.Range.ScrollingRange.SideMarginsEnabled = True
+			xyDiagram1.AxisY.Range.SideMarginsEnabled = True
+			xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+			Me.chartControl1.Diagram = xyDiagram1
+			Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.chartControl1.Legend.Visible = False
+			Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+			Me.chartControl1.Name = "chartControl1"
+			pointSeriesLabel1.LineVisible = True
+			pointSeriesLabel1.Visible = False
+			series1.Label = pointSeriesLabel1
+			series1.Name = "Series 1"
+			series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4, seriesPoint5})
+			series1.View = splineAreaSeriesView1
+			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
+			sideBySideBarSeriesLabel1.LineVisible = True
+			Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel1
+			Me.chartControl1.Size = New System.Drawing.Size(284, 194)
+			Me.chartControl1.TabIndex = 0
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(284, 194)
+			Me.Controls.Add(Me.chartControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(splineAreaSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(series1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private chartControl1 As DevExpress.XtraCharts.ChartControl
+	End Class
+End Namespace
+
